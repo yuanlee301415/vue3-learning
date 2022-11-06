7b97e0a40a3c2690674441a4a64c79ea45454e8d
yuanlee <yuanlee@163.com>
2022-11-06 21:10:52 +0800

    Done: 通过环境变量设置端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import type { UserConfig, ConfigEnv } from 'vite';
 
-import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({command, mode}: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+  const env = loadEnv(mode, root);
+  const { VITE_PORT } = env
+
+  return {
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      port: Number(VITE_PORT)
     }
   }
 })
