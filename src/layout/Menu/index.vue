9c5889c1c6136cee95312956a9e14cb218e0c184
yuanlee <yuanlee@163.com>
2022-11-17 20:43:18 +0800

    Done: add & use Less

--- src/layout/Menu/index.vue
+++ src/layout/Menu/index.vue
@@ -1,13 +1,11 @@
 <template>
-  <nav>
-    <MenuItem
-      v-for="item of basicRoutes"
-      :key="item.name"
-      :item="item"
-      :depth="1"
-      :fullPath="item.path"
-    />
-  </nav>
+  <MenuItem
+    v-for="item of basicRoutes"
+    :key="item.name"
+    :item="item"
+    :depth="1"
+    :fullPath="item.path"
+  />
 </template>
 
 <script lang="ts" setup>
@@ -15,16 +13,3 @@ import { basicRoutes } from "@/router/routes";
 import MenuItem from "@/layout/Menu/MenuItem.vue";
 // console.log('basicRoutes:', basicRoutes)
 </script>
-
-<style scoped>
-nav {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  overflow: auto;
-  border-right: 1px solid #999;
-  background-color: #001529;
-}
-</style>
